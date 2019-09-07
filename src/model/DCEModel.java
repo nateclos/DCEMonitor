d44699aad83caad32d67a4e360070bb4ebac5c1f
nateclos <nclos@email.arizona.edu>
2019-09-06 22:30:44 -0700

    Started parsing.

--- src/model/DCEModel.java
+++ src/model/DCEModel.java
@@ -1,22 +1,48 @@
 package model;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class DCEModel {
 	
 	File file;
 	
+	
+	
+	HashMap<String, Pdu> pdus = new HashMap<String, Pdu>();
+	
+	
+	
 	public DCEModel(File f) throws FileNotFoundException {
 		
 		this.file = f;
 		Scanner s = new Scanner(file);
-		String[] line = s.nextLine().split(",");
-		System.out.println(Arrays.toString(line));
-		line = s.nextLine().split(",");
-		System.out.println(Arrays.toString(line));
+		
+		s.nextLine();
+		
+		while(s.hasNextLine()) {
+			
+			String line = s.nextLine();
+			
+			System.out.println(line);
+			
+			String [] elements = line.split(",");
+			
+			String name = elements[0];
+			
+			String datetime = elements[5] + ' ' + elements[6];
+			
+			float amps = Float.valueOf(elements[7].substring(1,elements[7].length() - 1));
+			
+			System.out.println(amps);
+			
+			
+		}
 	}
 	
 	private class Pdu {
